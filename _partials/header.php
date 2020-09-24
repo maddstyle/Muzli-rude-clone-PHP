146248f9701379180025f6b0279ea30d279e37a4
maddstyle <michaela.decamara@gmail.com>
2020-09-24 10:53:04 -0400

    main partials created

---
+++ _partials/header.php
@@ -0,0 +1,12 @@
+<?php
+
+echo '<header class="site-header">
+        <nav class="container">
+	        <ul class="menu">
+	          <li><a href="index.php">Home</li>
+			      <li><a href="blog.php">Blog</a></li>
+			      <li><a href="contact.php">Contact</a></li>
+			      <li><a href="gallery.php">Gallery</a></li>
+		      </ul>
+	      </nav>
+      </header>';
